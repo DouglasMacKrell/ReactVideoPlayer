2759ab88e762d7c01649e189b819b18608a84070
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-08-15 17:15:10 -0400

    boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReactPlayer from 'react-player';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
